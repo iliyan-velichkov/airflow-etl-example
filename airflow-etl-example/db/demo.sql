c578f439c58183153bde258f6fb23a47b0c4fd3a
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-01-28 18:43:22 +0200

    add demo data - 100 000 entries

--- airflow-etl-example/db/demo.sql
+++ airflow-etl-example/db/demo.sql
@@ -1,22 +1,8 @@
--- Source table
-CREATE TABLE source_students (
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50)
-);
-
-INSERT INTO source_students(first_name, last_name)
-VALUES 
-    ('John', 'Doe'),
-    ('Jane', 'Smith');
-
 SELECT * FROM source_students;
 
--- Destination table
-CREATE TABLE target_students (
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50)
-);
+SELECT COUNT(*) FROM source_students;
+
 
 SELECT * FROM target_students;
+
+SELECT COUNT(*) FROM target_students;
