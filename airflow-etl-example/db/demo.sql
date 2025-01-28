f8643fc596b4a516ae6fec0605871fceb82b80ea
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-01-28 18:48:03 +0200

    update demo.sql

--- airflow-etl-example/db/demo.sql
+++ airflow-etl-example/db/demo.sql
@@ -1,8 +1,9 @@
+-- source_students table
 SELECT * FROM source_students;
 
 SELECT COUNT(*) FROM source_students;
 
-
-SELECT * FROM target_students;
+-- target_students table
+SELECT * FROM target_students ORDER BY id;
 
 SELECT COUNT(*) FROM target_students;
