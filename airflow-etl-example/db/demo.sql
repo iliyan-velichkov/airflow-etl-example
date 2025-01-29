5fdf0bea4e22a6ce5899d58fdbc5aa6edaceec5f
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-01-29 09:34:54 +0200

    add 1 million demo entries - 21 MB

--- airflow-etl-example/db/demo.sql
+++ airflow-etl-example/db/demo.sql
@@ -1,9 +1,17 @@
 -- source_students table
-SELECT * FROM source_students;
+SELECT * FROM source_students ORDER by id LIMIT 10;
 
 SELECT COUNT(*) FROM source_students;
 
 -- target_students table
-SELECT * FROM target_students ORDER BY id;
+SELECT * FROM target_students ORDER by id LIMIT 10;
 
 SELECT COUNT(*) FROM target_students;
+
+-- join two tables
+SELECT s.id, s.first_name as source_first_name, t.first_name as target_first_name, s.last_name as source_last_name, t.last_name as target_last_name
+FROM source_students as s
+INNER JOIN target_students as t
+ON s.id=t.id
+ORDER by id
+LIMIT 10;
